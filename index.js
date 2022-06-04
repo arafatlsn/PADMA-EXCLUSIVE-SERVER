81a65c511a476b827465dc266a043525183a55f8
Arafat37 <arafatsarkar22@gmail.com>
2022-06-04 13:30:25 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.moy4n.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+
+  try{
+
+    await client.connect()
+    const destinationCollection = client.db('Padma_Exclusive').collection('destinations')
+
+    // load all destinations 
+    app.get('/destinations', async(req, res) => {
+      const result = await destinationCollection.find({}).toArray();
+      res.send(result)
+    })
+
+
+  }
+  finally{
+
+  }
+
+
+}
+run()
+
+
+
+app.get('/', async(req, res) => {
+  res.send('hello world')
+})
+
+app.listen(port, () => {
+  console.log('server is running on', port)
+})
