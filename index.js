4de8faff047cbc94930177422ec157d856ed212a
Arafat37 <arafatsarkar22@gmail.com>
2022-06-05 13:00:47 +0600

    load single destination by id

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const { json } = require('express/lib/response');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.moy4n.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -31,6 +29,14 @@ async function run(){
     app.get('/destination', async(req, res) => {
       const destinationInfo = JSON.parse(req.headers?.destinationinfo)
       const query = { from: destinationInfo?.from, to: destinationInfo?.to }
+      const result = await destinationCollection.findOne(query);
+      res.send(result)
+    })
+
+    // load single destination by id 
+    app.get('/destinationid', async(req, res) => {
+      const destinationId = req.query?.destinationId;
+      const query = { _id: ObjectId(destinationId) };
       const result = await destinationCollection.findOne(query);
       res.send(result)
     })
