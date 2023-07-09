1129010b9b1d3663ec92ba26aca7e5dc2d4072be
ArafatLsn <arafatsarkar22@gmail.com>
2023-07-09 23:07:28 +0600

    updated

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-const routerDestination = require("./app/modules/destinations/destination.router");
-const routerTickets = require("./app/modules/tickets/tickets.router");
-
 const express = require("express");
 const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+const port = 5000;
+const routerDestination = require("./app/modules/destinations/destination.router");
+const routerTickets = require("./app/modules/tickets/tickets.router");
 const bookingsRouter = require("./app/modules/bookings/bookings.router");
 const paymentRouter = require("./app/modules/payment/payment.router");
 
-const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/destinations", routerDestination);
 app.use("/api/v1/tickets", routerTickets);
 app.use("/api/v1/bookings", bookingsRouter);
-app.use("/api/v1/payment", paymentRouter)
+app.use("/api/v1/payment", paymentRouter);
 
-module.exports = app;
+dbConnect().catch((err) => console.log(err));
+
+async function dbConnect() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/padma-exclusive");
+    app.listen(port, () => {
+      console.log("Server is running!");
+    });
+  } catch (err) {
+    console.log(err?.message);
+  }
+}
